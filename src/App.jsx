871fb976a809fd56c8225e6631a44482138c965d
nanditha <nanditha.daram3@gmail.com>
2025-11-07 22:38:57 +0530

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+ import {BrowserRouter as Router, Route, Routes, BrowserRouter} from 'react-router-dom';
+ import React from 'react'
 
 export default function App() {
   return (
-    <div className='text-red-300'>
-      App
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<div>Home Page</div>} />
+      <Route path="/about" element={<div>About Page</div>} /> 
+      <Route path="/signin" element={<div>Signin Page</div>} />
+      <Route path="/signout" element={<div>Signout Page</div>} />
+      <Route path="/profile" element={<div>Profile Page</div>} />
+    </Routes>
+    </BrowserRouter>
   )
 }
