5bd8085deeffbe2912ab8d0e18852f07c389fdc1
nanditha <nanditha.daram3@gmail.com>
2025-11-07 23:22:55 +0530

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
  import {BrowserRouter as Router, Route, Routes, BrowserRouter} from 'react-router-dom';
  import React from 'react'
-
+import Header from './components/Header.jsx';
+import signout from './pages/signout.jsx';
+import signin from './pages/signin.jsx';
+import profile from './pages/profile.jsx';
+import about from './pages/about.jsx';
+import home from './pages/home.jsx';
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<div>Home Page</div>} />
       <Route path="/about" element={<div>About Page</div>} /> 
